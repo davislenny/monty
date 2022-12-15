fc86e770bf640534f74ee7aa5cee410175b19d5f
davislenny <davidlmwangi7@gmail.com>
2022-12-15 14:22:22 -0800

    Add opcode2_func.c

---
+++ opcode2_func.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - literally does nothing
+ * @stack: pointer to the top of the stack
+ * @line_number: the index of the current line
+ *
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
