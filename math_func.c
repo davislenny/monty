a56ccf68d6ab7909e0ee36e52ed8aef9ac94f793
davislenny <davidlmwangi7@gmail.com>
2022-12-15 14:23:19 -0800

    Add math_func.c

---
+++ math_func.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * _add - adds top of stack and second top of stack
+ *
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%d: can't add, stack too short\n", line_number);
+		error_exit(stack);
+	}
+	(*stack)->next->n += (*stack)->n;
+	_pop(stack, line_number);
+}
+
+/**
+ * _sub - subtracts top of stack and second top of stack
+ *
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%d: can't sub, stack too short\n", line_number);
+		error_exit(stack);
+	}
+	(*stack)->next->n -= (*stack)->n;
+	_pop(stack, line_number);
+}
