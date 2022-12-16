3af51b760edc4cb52a08d4b5405b93a5af9786f7
davislenny <davidlmwangi7@gmail.com>
2022-12-16 10:23:48 -0800

    Add opcode2_func.c

--- opcode2_func.c
+++ opcode2_func.c
@@ -10,3 +10,39 @@ void _nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+/**
+ * _pchar - prints a character at the top of stack
+ * @stack: points to linked list stack_t
+ * @line_number: line opcode occurs
+ */
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty", line_number);
+		error_exit(stack);
+	}
+	else if (!isprint((*stack)->n))
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range", line_number);
+		error_exit(stack);
+	}
+	printf("%c\n", (*stack)->n);
+}
+/**
+ * _pstr - prints a string stored in stack
+ * @stack: poins to linked list stack_t
+ * @line_number: line opcode occurs
+ */
+/*void _pstr(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+		putchar('\n');
+	while ((*stack)->next)
+	{
+		printf("%c", (*stack)->n);
+		if ((*stack)->next->n == 0 || !isprint((*stack)->next->n))
+			break;
+	}
+	putchat('\n');
+*/
