378f9f585c9b82ce64814574a49d724821aea62c
davislenny <davidlmwangi7@gmail.com>
2022-12-16 07:56:57 -0800

    Add opcode_func.c

--- opcode_func.c
+++ opcode_func.c
@@ -88,7 +88,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		error_exit(*stack);
+		error_exit(stack);
 	}
 	delete_dnodeint_at_index(stack, 0);
 }
