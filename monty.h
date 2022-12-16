a8f37691019d4d73052e4502f42d33857c7b09a7
davislenny <davidlmwangi7@gmail.com>
2022-12-16 09:02:22 -0800

    Add header file

--- monty.h
+++ monty.h
@@ -38,21 +38,37 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* monty_func module */
 char *parse_line(char *line);
 void (*get_op_func(char *str))(stack_t **stack, unsigned int line_number);
 void read_file(char *filename, stack_t **stack);
+
+/* opcode_func */
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
-void _add(stack_t **stack, unsigned int line_number);
+
+/* opcode2_func */
 void _nop(stack_t **stack, unsigned int line_number);
+
+/* math_func */
+void _add(stack_t **stack, unsigned int line_number);
 void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+
+/* stack_t_functions */
 stack_t *add_dnodeint_end(stack_t **head, const int n);
 stack_t *add_dnodeint(stack_t **head, const int n);
 void free_dlistint(stack_t *head);
 int delete_dnodeint_at_index(stack_t **head, unsigned int index);
+
+/*  helper functions */
 void error_exit(stack_t **stack);
 int isnumber(char *str);
-#endif
+
+
+#endif /* MONTY_H */
